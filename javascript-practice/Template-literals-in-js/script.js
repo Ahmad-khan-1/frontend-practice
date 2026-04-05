72ec29e7ea3258ae5ffa8098d38f8dbd98524a5d
Sayed Mojtaba Qureshi <qurayshi246@gmail.com>
2026-04-05 11:03:59 +0500

    concept of Template literals in js

---
+++ javascript-practice/Template-literals-in-js/script.js
@@ -0,0 +1,20 @@
+//Template literals are a modern way to write strings in JavaScript using backticks instead of quotes.
+
+//ols WAY
+const name = "Ali"
+console.log("Hello"+" "+name);
+
+//new way of writing string 
+const name1 = "Sara"
+console.log(`Hello ${name1}`);
+
+
+const a = 4 
+const b = 10 
+console.log(`sum of a +b  is ${a+b}`);
+
+//inside function 
+function greet () {
+    return "Hello"
+}
+console.log(`${greet()} Aman`);
